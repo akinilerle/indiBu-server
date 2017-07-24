d898d260d525db5d7ff87dc4ec04da1c23d2511b
ailerle <akinilerle@gmail.com>
2017-07-24 14:55:39 +0300

    initial project setup indent correction Change-Id: Ide773bf80bb0d1372ead333c8620d72efe719927

---
+++ src/main/java/com/indibu/indiBuserver/IndiBuServerApplication.java
@@ -0,0 +1,12 @@
+package com.indibu.indiBuserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndiBuServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndiBuServerApplication.class, args);
+    }
+}
